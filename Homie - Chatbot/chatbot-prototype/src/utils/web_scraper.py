5554e2cfa3f5850fed03ee6e01e0b46ad8f49ead
alexanbennett <kiaidbennett@gmail.com>
2025-03-20 10:36:44 +0000

    Delete Homie - Chatbot/chatbot-prototype directory

--- Homie - Chatbot/chatbot-prototype/src/utils/web_scraper.py
+++
@@ -1,8 +0,0 @@
-class WebScraper:
-    def __init__(self):
-        pass
-
-    def scrape_links(self, criteria):
-        # This method will implement the web scraping logic to find house listings
-        # based on the provided criteria. It will return a list of relevant links.
-        pass
